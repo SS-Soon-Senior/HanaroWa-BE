6d3d4d0852941c3395fbeb823e45c06167f99a82
unknown <super_vet@naver.com>
2025-08-19 16:59:04 +0900

    feature/Branch&Location entity

--- src/main/java/com/ss/hanarowa/branch/entity/Branch.java
+++ src/main/java/com/ss/hanarowa/branch/entity/Branch.java
@@ -1,4 +1,43 @@
 package com.ss.hanarowa.branch.entity;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Branch {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private String address;
+
+	@Column(nullable = false)
+	private String telNumber;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "location",
+		foreignKey = @ForeignKey(name="fk_Branch_Location"))
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	private Location location;
 }
