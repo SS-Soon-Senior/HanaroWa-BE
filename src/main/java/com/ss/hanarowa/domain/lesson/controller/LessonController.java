19640238bf006fc271e5eb5928a7c51bd12ed8a5
unknown <super_vet@naver.com>
2025-08-28 10:11:49 +0900

    feat: 사용자 강좌 상세보기

--- src/main/java/com/ss/hanarowa/domain/lesson/controller/LessonController.java
+++ src/main/java/com/ss/hanarowa/domain/lesson/controller/LessonController.java
@@ -1,8 +1,39 @@
 package com.ss.hanarowa.domain.lesson.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ss.hanarowa.domain.lesson.dto.request.ReviewRequestDTO;
+import com.ss.hanarowa.domain.lesson.dto.response.LessonMoreDetailResponseDTO;
+import com.ss.hanarowa.domain.lesson.service.LessonService;
+import com.ss.hanarowa.domain.lesson.service.ReviewService;
+import com.ss.hanarowa.domain.member.entity.Member;
+import com.ss.hanarowa.domain.member.repository.MemberRepository;
+import com.ss.hanarowa.global.exception.GeneralException;
+import com.ss.hanarowa.global.response.ApiResponse;
+import com.ss.hanarowa.global.response.code.status.ErrorStatus;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Tag(name="[사용자] 강좌")
+@RestController
+@RequestMapping("/lesson")
+@RequiredArgsConstructor
 public class LessonController {
 	private final ReviewService reviewService;
 	private final MemberRepository memberRepository;
+	private final LessonService lessonService;
 
 	@PostMapping("/lesson/{lessonId}/review")
 	@Operation(summary = "강좌 리뷰 작성", description = "사용자가 특정 강좌에 대한 리뷰를 작성합니다.")
@@ -19,9 +50,7 @@ public class LessonController {
 		return ResponseEntity.ok(ApiResponse.onSuccess(null));
 	}
 
-	private final LessonService lessonService;
-
-	@Operation(summary="관리자 강좌 상세 보기")
+	@Operation(summary="사용자 강좌 상세 보기")
 	@GetMapping("/{lessonId}")
 	public ResponseEntity<ApiResponse<LessonMoreDetailResponseDTO>> getLessonDetail(@PathVariable Long lessonId){
 		return ResponseEntity.ok(ApiResponse.onSuccess(lessonService.getLessonMoreDetail(lessonId)));
