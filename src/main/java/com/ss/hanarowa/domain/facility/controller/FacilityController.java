0d4fd626d9197e79fec90620c5d743473c7bd821
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2025-08-28 15:25:44 +0900

    feat: swagger 설정

--- src/main/java/com/ss/hanarowa/domain/facility/controller/FacilityController.java
+++ src/main/java/com/ss/hanarowa/domain/facility/controller/FacilityController.java
@@ -19,6 +19,7 @@ import com.ss.hanarowa.domain.facility.service.FacilityService;
 import com.ss.hanarowa.domain.member.service.MemberService;
 import com.ss.hanarowa.global.response.ApiResponse;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,33 +28,34 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/facility")
 @RequiredArgsConstructor
+@Tag(name = "[사용자] 시설", description = "사용자 시설 관련 API")
 public class FacilityController {
 	private final FacilityService facilityService;
 	private final MemberService memberService;
 
 	@GetMapping("/{branchId}")
-	@Tag(name = "시설 리스트 목록")
+	@Operation(summary = "시설 리스트 목록 API", description = "시설 리스트 목록을 조회합니다.")
 	public ResponseEntity<ApiResponse<List<FacilityResponseDTO>>> getFacilityByBranchId(@PathVariable Long branchId) {
 		List<FacilityResponseDTO> list = facilityService.getAllFacilities(branchId);
 		return ResponseEntity.ok(ApiResponse.onSuccess(list));
 	}
 
 	@GetMapping("/detail/{facilityId}")
-	@Tag(name = "시설 상세보기")
+	@Operation(summary = "시설 상세보기", description = "시설 상세를 조회합니다.")
 	public ResponseEntity<ApiResponse<FacilityDetailResponseDTO>> getDetailFacility(@PathVariable Long facilityId) {
 		FacilityDetailResponseDTO facilityDetailResponseDTO = facilityService.getDetailFacility(facilityId);
 		return ResponseEntity.ok(ApiResponse.onSuccess(facilityDetailResponseDTO));
 	}
 
 	@DeleteMapping("/{reservationId}")
-	@Tag(name = "시설 예약 취소")
+	@Operation(summary = "시설 예약 취소", description = "시설 예약을 취소합니다.")
 	public ResponseEntity<ApiResponse<Void>> deleteFacilityReservation(@PathVariable Long reservationId){
 		facilityService.deleteFacilityReservation(reservationId);
 		return ResponseEntity.ok(ApiResponse.onSuccess(null));
 	}
 
 	@PostMapping("/reservation")
-	@Tag(name = "시설 예약하기")
+	@Operation(summary = "시설 예약하기", description = "시설을 예약합니다.")
 	public ResponseEntity<ApiResponse<Void>> reservateFacility(@RequestBody FacilityReservationDTO facilityReservationDTO, Authentication authentication){
 		String email = authentication.getName();
 		Long memberId = memberService.getMemberIdByEmail(email);
