43a05626ed48f5b88ca5bc8cd94533ce6dca5aaa
doormoonn <robort123@gachon.ac.kr>
2025-08-28 19:24:58 +0900

    controller 추가...

---
+++ src/main/java/com/ss/hanarowa/domain/member/controller/JwtRefreshController.java
@@ -0,0 +1,56 @@
+package com.ss.hanarowa.domain.member.controller;
+
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ss.hanarowa.global.exception.GeneralException;
+import com.ss.hanarowa.global.response.code.status.ErrorStatus;
+import com.ss.hanarowa.global.security.JwtUtil;
+import com.ss.hanarowa.global.security.exception.CustomJwtException;
+
+@RestController
+public class JwtRefreshController {
+	@PostMapping("/member/refresh")
+	public ResponseEntity<Map<String, Object>> refresh(
+		@RequestHeader("Authorization") String authHeader,
+		@RequestParam String refreshToken) {
+		if (refreshToken == null)
+			throw new GeneralException(ErrorStatus.TOKEN_NOT_FOUND);
+
+		if (authHeader == null || authHeader.length() < 7)
+			throw new GeneralException(ErrorStatus.TOKEN_INVALID);
+
+		String accessToken = authHeader.substring(7);
+		if (!didExpireToken(accessToken)) {
+			return ResponseEntity.ok(Map.of("accessToken", accessToken, "refreshToken", refreshToken));
+		}
+
+		Map<String, Object> claim = JwtUtil.validateToken(refreshToken);
+		String newAccessToken = JwtUtil.generateToken(claim, 10);
+		String newRefreshToken = isSomeLeftTime((long)claim.get("exp"))
+			? JwtUtil.generateToken(claim, 60 * 24)
+			: refreshToken;
+
+		return ResponseEntity.ok(Map.of("accessToken", newAccessToken, "refreshToken", newRefreshToken));
+
+	}
+
+	private boolean isSomeLeftTime(long exp) {
+		long nowSec = System.currentTimeMillis() / 1000;
+		return (exp - nowSec) < 60 * 60;
+	}
+
+	private boolean didExpireToken(String accessToken) {
+		try {
+			JwtUtil.validateToken(accessToken);
+		} catch (CustomJwtException e) {
+			return true;
+		}
+		return false;
+	}
+}
