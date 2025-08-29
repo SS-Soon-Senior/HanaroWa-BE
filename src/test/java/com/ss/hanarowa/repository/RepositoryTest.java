b4549043d53ec64af9294e5c89adc42eb0f4f5da
unknown <super_vet@naver.com>
2025-08-29 15:33:41 +0900

    feat: BranchRepositoryTest

--- src/test/java/com/ss/hanarowa/repository/RepositoryTest.java
+++ src/test/java/com/ss/hanarowa/repository/RepositoryTest.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class RepositoryTest {
+public class RepositoryTest {
 	protected <T> void print(T entity) {
 		System.out.println(entity);
 	}
