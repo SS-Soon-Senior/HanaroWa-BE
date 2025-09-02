9471548fc5106db1d668ad8e0fc7caacc059ffa3
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2025-09-02 16:08:27 +0900

    fix: Reissue 시 토큰 쿠키로 response

--- src/main/java/com/ss/hanarowa/global/config/CustomSecurityConfig.java
+++ src/main/java/com/ss/hanarowa/global/config/CustomSecurityConfig.java
@@ -53,6 +53,7 @@ public class CustomSecurityConfig {
 			.httpBasic(AbstractHttpConfigurer::disable)
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers(
+					"/auth/reissue",
 					"/member/regist",
 					"/auth/signin",
 					"/auth/signin/**",
