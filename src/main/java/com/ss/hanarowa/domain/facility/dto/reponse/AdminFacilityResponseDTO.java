cb79c6b411dffa1b8baa0666ebf64398d4bed163
“1siis1” <sisees01@gmail.com>
2025-09-03 17:48:52 +0900

    feat:관리자 시설 예약내역 목록 관련 DTO 수정 및 함수 추가

--- src/main/java/com/ss/hanarowa/domain/facility/dto/reponse/AdminFacilityResponseDTO.java
+++ src/main/java/com/ss/hanarowa/domain/facility/dto/reponse/AdminFacilityResponseDTO.java
@@ -17,7 +17,7 @@ public class AdminFacilityResponseDTO {
 	private String memberName; // 예약자 이름
 	private String memberEmail; // 예약자 이메일₩
 	private String branchName; // 지점명
-	private String locationName; // 위치명
-	private LocalDateTime startedAt; // 예약 시작 사간
-	private LocalDateTime endedAt; // 예약 종료 시간
+	private String startedAt; // 예약 시작 사간
+	private String reservedAt; //예약 한 시간
+	private Boolean isUsed; // 사용 유무
 }
